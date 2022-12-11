a4a2570939c71e315c35ba887c6a59b27f48d23e
JuanAntonioMN <106030895+JuanAntonioMN@users.noreply.github.com>
2022-12-11 14:59:46 -0600

    Add files via upload

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
-<!DOCTYPE html>
+<! DOCTYPE html>
 <html lang="es">
-<head>
+<cabeza>
     <meta charset="UTF-8">
     <title>Inicio</title>
-    <link rel="shortcut icon"  href="img/logoi.ico" >
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    <link rel="icono de acceso directo" href="img/logoi.ico" >
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anónimo"></script >
 
-</head>
+</cabeza>
 
-</html>
+</.html>
 <?php
 include "menu.php";
  
